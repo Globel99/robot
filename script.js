38a1615a6251a7c0ad277ec13b6364152f1e3127
Levente Kapitany <levente.kapitany@logmein.com>
2021-11-17 05:38:56 +0100

    asd

--- script.js
+++ script.js
@@ -1,151 +1,141 @@
-const baudRate = 19200
-
-port = null
-writer = null
-
-gamepad = null
-buttonsCache = []
-buttonsDiff = []
-
-writer = null
-reader = null
-inputTextDecoder = new TextDecoderStream()
-
-readValueFull = ""
-
-if ("serial" in window.navigator) {
-
-} else {
-  document.getElementById('error').style.display = 'block'
+if (!('serial' in window.navigator && TextDecoderStream)) {
+  setErrorScreen();
 }
 
+const baudRate = 19200;
 
+port = null;
 
-setInterval(() => {
-  if (gamepad) {
-    if (buttonsCache === []) {
-      buttonsCache = window.navigator.getGamepads()[0].buttons
-    } else {
-      const buttons = window.navigator.getGamepads()[0].buttons
+writer = null;
+reader = null;
+inputTextDecoder = new TextDecoderStream();
 
-      buttons.forEach((button, index) => {
-        if (button.value > 0) console.log(index)
-      })
-
-      buttonsCache = buttons
-    }
-  }
-}, 100)
-
-
+readValueFull = '';
 
 async function connect() {
-  await selectPort()
+  await selectPort();
 }
 
 async function selectPort() {
   port = await navigator.serial.requestPort();
-  await port.open({ baudRate: baudRate });
+  await port.open({ baudRate });
 
-  setConnectedState()
+  setConnectedState();
 
   const encoder = new TextEncoderStream();
   outputDone = encoder.readable.pipeTo(port.writable);
-  console.log(outputDone)
   outputStream = encoder.writable;
   writer = outputStream.getWriter();
 
-  port.readable.pipeTo(inputTextDecoder.writable)
-  reader = inputTextDecoder.readable.getReader()
+  port.readable.pipeTo(inputTextDecoder.writable);
+  reader = inputTextDecoder.readable.getReader();
 
-  setInterval(readLoop, 100)
+  setInterval(readLoop, 100);
 }
 
-function onRangeChange(motor, event) {
-  sendSingleInput(motor, event.target.value);
+function onAngleChange(motor, value) {
+  startSingleControl(motor, value);
+  setAngleLabel(motor, value);
 }
 
-function setConnectedState() {
-  document.getElementById('buttonContainer').style.display = 'none';
-
-  const inputs = document.querySelectorAll('input')
-  inputs.forEach(input => input.disabled = false)
+function sendNumber(number) {
+  writer.write(new Uint8Array([number]));
+  writer.write('x');
+  updateConsole('outputConsole', number);
 }
 
-async function sendNumber(number) {
-  writer.write(new Uint8Array([number]))
-  closeSend(number)
+function sendString(string) {
+  writer.write(string);
+  writer.write('x');
+  updateConsole('outputConsole', string);
 }
-
-async function sendString(string) {
-  writer.write(string)
-  closeSend(string)
-}
-
-async function closeSend(message) {
-  writer.write('x')
-  updateConsole('outputConsole', message)
-}
-
-window.addEventListener("gamepadconnected", function (e) {
-  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    e.gamepad.index, e.gamepad.id,
-    e.gamepad.buttons.length, e.gamepad.axes.length);
-
-  console.log(e.gamepad)
-  gamepad = e.gamepad
-
-  document.getElementById('controller-overlay').style.display = 'block'
-});
 
 async function readLoop() {
   const { value, done } = await reader.read();
   if (done) {
     reader.releaseLock();
   }
-
-  readValueFull += value
+  readValueFull += value;
 
   if (value[value.length - 1] === '\n') {
-    console.log(readValueFull)
-    updateConsole("inputConsole", readValueFull)
-    readValueFull = ""
+    updateConsole('inputConsole', readValueFull);
+    readValueFull = '';
   }
 }
 
 function updateConsole(consoleId, value) {
-  const console = document.getElementById(consoleId)
+  const console = document.getElementById(consoleId);
 
-  if (console.innerText.length > 1000) console.innerText = console.innerText.slice(800);
-  console.innerText += value
-  if (consoleId == "outputConsole") console.innerHTML += '<br>'
+  if (console.innerText.length > 1000)
+    console.innerText = console.innerText.slice(800);
+  console.innerText += value;
+  if (consoleId == 'outputConsole') console.innerHTML += '<br>';
 }
 
 function clearConsole(consoleId) {
-  const console = document.getElementById(consoleId)
-  console.innerHTML = ''
+  const console = document.getElementById(consoleId);
+  console.innerHTML = '';
 }
 
 function sendManualInput() {
-  const message = document.getElementById('inputField').value
-  sendString(message)
+  const message = document.getElementById('inputField').value;
+  sendString(message);
 }
 
-function sendParallelInputs() {
-  const motors = document.getElementById('parallelInputs').querySelectorAll('div')
+function getParalellControlParameters() {
+  const motors = document
+    .getElementById('parallelInputs')
+    .querySelectorAll('div');
 
-  sendString('parallel')
-  motors.forEach(motor => {
-    const angle = motor.querySelectorAll('input')[0].value
-    const travel = motor.querySelectorAll('input')[1].value
-
-    sendNumber(angle)
-    sendNumber(travel)
-  })
+  return motors.map((motor) => ({
+    angle: motor.querySelectorAll('input')[0].value,
+    travelTime: motor.querySelectorAll('input')[1].value,
+  }));
 }
 
-function sendSingleInput(motor, target) {
-  sendString('single')
-  sendNumber(motor)
-  sendNumber(target)
+function startParallelControl() {
+  const parameters = getParalellControlParameters();
+
+  parameters.forEach((motor) => {
+    sendString('parallel');
+    sendNumber(motor.angle);
+    sendNumber(motor.travelTime);
+  });
+}
+
+function startSingleControl(motor, target) {
+  sendString('single');
+  sendNumber(motor);
+  sendNumber(target);
+}
+
+function setErrorScreen() {
+  document.getElementById('singleControl').style.display = 'none';
+  document.getElementById('parallelControl').style.display = 'none';
+  document.getElementById('error').style.display = 'block';
+}
+
+function setConnectedState() {
+  document.getElementById('buttonContainer').style.display = 'none';
+
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach((input) => (input.disabled = false));
+}
+
+function setAngleLabel(motor, value) {
+  document.getElementsByClassName()[motor - 1].innerHTML = value;
+}
+
+function setSingleControlScreen() {
+  document.getElementById('parallelControl').setAttribute('hidden', true);
+  document.getElementById('singleControl').removeAttribute('hidden');
+  document.getElementById('singleSwitch').setAttribute('disabled', true);
+  document.getElementById('parallelSwitch').removeAttribute('disabled');
+}
+
+function setParallelControlScreen() {
+  document.getElementById('singleControl').setAttribute('hidden', true);
+  document.getElementById('parallelControl').removeAttribute('hidden');
+  document.getElementById('singleSwitch').removeAttribute('disabled');
+  document.getElementById('parallelSwitch').setAttribute('disabled', true);
 }
